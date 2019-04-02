59e6cb57c7c8f81897b2bc2af968582e937cc2ec
Uni <1025687605@qq.com>
2019-04-02 10:10:21 +0800

    chore: add config

--- jest.config.js
+++ jest.config.js
@@ -3,22 +3,19 @@ module.exports = {
     '.(ts|tsx)': 'ts-jest',
   },
   moduleNameMapper: {
-    "\\.(css|less|scss)$": "identity-obj-proxy"
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
   },
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/'],
   testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  setupFilesAfterEnv: ["<rootDir>/__mocks__/setup.ts"],
+  setupFilesAfterEnv: ['<rootDir>/__mocks__/setup.ts'],
   coverageThreshold: {
     global: {
       statements: 80,
       branches: 80,
       lines: 80,
       functions: 80,
-    }
+    },
   },
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ]
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 }
