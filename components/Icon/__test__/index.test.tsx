15829dc206d345101d35a6e0cfe953036d7e42e5
Uni <1025687605@qq.com>
2019-04-07 22:55:06 +0800

    test: add icon tezt

--- components/Icon/__test__/index.test.tsx
+++ components/Icon/__test__/index.test.tsx
@@ -4,15 +4,19 @@ import Icon from '../index'
 
 describe('icon test', () => {
   it('snapshot', () => {
-    const { container } = render(<Icon type="eye-close"/>)
-    const { container: contaner1 } = render(<Icon type="eye-close" size={20} color="red"/>)
+    const { container } = render(<Icon type="eye-close" />)
+    const { container: contaner1 } = render(
+      <Icon type="eye-close" size={20} color="red" rotate={90} spin={false} />
+    )
+    const { container: contaner2 } = render(<Icon type="eye-close" prefixCls="uni-design" />)
     expect(container.innerHTML).toMatchSnapshot()
     expect(contaner1.innerHTML).toMatchSnapshot()
+    expect(contaner2.innerHTML).toMatchSnapshot()
   })
   it('fire click', () => {
     const fn = jest.fn()
-    const { container } = render(<Icon type="eye-close" onClick={fn}/>)
-    fireEvent.click(container.firstChild as any)
+    const { container } = render(<Icon type="eye-close" onClick={fn} />)
+    fireEvent.click(container.firstChild as HTMLElement)
     expect(fn).toBeCalled()
   })
 })
