c5d3b45bbf2638f811eb9918b0650a50f6dcdd0e
Muyunyun <328375795@qq.com>
2019-04-20 21:04:19 +0800

    chore: 站点重构

---
+++ site/theme/template/IconDisplay/index.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import manifest from '@ant-design/icons/lib/manifest';
+import { ThemeType as ThemeFolderType } from '@ant-design/icons/lib/types';
+// import { Radio, Icon, Input } from 'antd';
+import { RadioChangeEvent } from 'antd/lib/radio/interface';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import debounce from 'lodash/debounce';
+import Category from './Category';
+import { FilledIcon, OutlinedIcon, TwoToneIcon } from './themeIcons';
+import { categories, Categories, CategoriesKeys } from './fields';
+import { ThemeType } from '../../../../components/icon';
+
+interface IconDisplayProps extends InjectedIntlProps {}
+
+interface IconDisplayState {
+  theme: ThemeType;
+  searchKey: string;
+}
+
+class IconDisplay extends React.Component<IconDisplayProps, IconDisplayState> {
+  static categories: Categories = categories;
+
+  static newIconNames: string[] = [];
+
+  static themeTypeMapper: { [key: string]: ThemeFolderType } = {
+    filled: 'fill',
+    outlined: 'outline',
+    twoTone: 'twotone',
+  };
+
+  state: IconDisplayState = {
+    theme: 'outlined',
+    searchKey: '',
+  };
+
+  constructor(props: IconDisplayProps) {
+    super(props);
+    this.handleSearchIcon = debounce(this.handleSearchIcon, 300);
+  }
+
+  getComputedDisplayList() {
+    return Object.keys(IconDisplay.categories)
+      .map((category: CategoriesKeys) => ({
+        category,
+        icons: IconDisplay.categories[category].filter(
+          name => manifest[IconDisplay.themeTypeMapper[this.state.theme]].indexOf(name) !== -1,
+        ),
+      }))
+      .filter(({ icons }) => Boolean(icons.length));
+  }
+
+  handleChangeTheme = (e: RadioChangeEvent) => {
+    this.setState({
+      theme: e.target.value as ThemeType,
+    });
+  };
+
+  handleSearchIcon = (searchKey: string) => {
+    this.setState(prevState => ({
+      ...prevState,
+      searchKey,
+    }));
+  };
+
+  renderCategories(list: Array<{ category: CategoriesKeys; icons: string[] }>) {
+    const { searchKey } = this.state;
+    return list
+      .map(({ category, icons }) => {
+        const iconResult = icons.filter(name => name.includes(searchKey));
+        if (iconResult.length === 0) {
+          return null;
+        }
+        return (
+          <Category
+            key={category}
+            title={category}
+            icons={iconResult}
+            theme={this.state.theme}
+            newIcons={IconDisplay.newIconNames}
+          />
+        );
+      })
+      .filter(category => !!category);
+  }
+
+  render() {
+    const {
+      intl: { messages },
+    } = this.props;
+    const list = this.getComputedDisplayList();
+    return (
+      <>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {/* <Radio.Group value={this.state.theme} onChange={this.handleChangeTheme} size="large">
+            <Radio.Button value="outlined">
+              <Icon component={OutlinedIcon} /> {messages['app.docs.components.icon.outlined']}
+            </Radio.Button>
+            <Radio.Button value="filled">
+              <Icon component={FilledIcon} /> {messages['app.docs.components.icon.filled']}
+            </Radio.Button>
+            <Radio.Button value="twoTone">
+              <Icon component={TwoToneIcon} /> {messages['app.docs.components.icon.two-tone']}
+            </Radio.Button>
+          </Radio.Group> */}
+          {/* <Input.Search
+            placeholder={messages['app.docs.components.icon.search.placeholder']}
+            style={{ marginLeft: 10, flex: 1 }}
+            allowClear
+            onChange={e => this.handleSearchIcon(e.currentTarget.value)}
+            size="large"
+            autoFocus
+          /> */}
+        </div>
+        {this.renderCategories(list)}
+      </>
+    );
+  }
+}
+
+export default injectIntl(IconDisplay);
---
+++ site/theme/template/Layout/index.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { enquireScreen } from 'enquire-js';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import 'moment/locale/zh-cn';
+// import { LocaleProvider } from 'antd';
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+// import zhCN from 'antd/lib/locale-provider/zh_CN';
+import Header from './Header';
+import enLocale from '../../en-US';
+import cnLocale from '../../zh-CN';
+import * as utils from '../utils';
+
+if (typeof window !== 'undefined' && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => registration.unregister());
+  });
+}
+
+if (typeof window !== 'undefined') {
+  /* eslint-disable global-require */
+  require('../../static/style');
+
+  // Expose to iframe
+  window.react = React;
+  window['react-dom'] = ReactDOM;
+  // window.antd = require('antd');
+  /* eslint-enable global-require */
+
+  // Error log statistic
+  window.addEventListener('error', function onError(e) {
+    // Ignore ResizeObserver error
+    if (e.message === 'ResizeObserver loop limit exceeded') {
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+    }
+  });
+
+  if (process.env.NODE_ENV === 'production') {
+    LogRocket.init('kpuw4z/ant-design');
+    setupLogRocketReact(LogRocket);
+  }
+}
+
+let isMobile = false;
+enquireScreen(b => {
+  isMobile = b;
+});
+
+export default class Layout extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object.isRequired,
+  };
+
+  static childContextTypes = {
+    isMobile: PropTypes.bool,
+  };
+
+  constructor(props) {
+    super(props);
+    const { pathname } = props.location;
+    const appLocale = utils.isZhCN(pathname) ? cnLocale : enLocale;
+    addLocaleData(appLocale.data);
+
+    this.state = {
+      appLocale,
+      isMobile,
+    };
+  }
+
+  getChildContext() {
+    const { isMobile: mobile } = this.state;
+    return { isMobile: mobile };
+  }
+
+  componentDidMount() {
+    const { router } = this.context;
+    router.listen(loc => {
+      if (typeof window.ga !== 'undefined') {
+        window.ga('send', 'pageview', loc.pathname + loc.search);
+      }
+      // eslint-disable-next-line
+      if (typeof window._hmt !== 'undefined') {
+        // eslint-disable-next-line
+        window._hmt.push(['_trackPageview', loc.pathname + loc.search]);
+      }
+    });
+
+    const nprogressHiddenStyle = document.getElementById('nprogress-style');
+    if (nprogressHiddenStyle) {
+      this.timer = setTimeout(() => {
+        nprogressHiddenStyle.parentNode.removeChild(nprogressHiddenStyle);
+      }, 0);
+    }
+
+    enquireScreen(b => {
+      this.setState({
+        isMobile: !!b,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    const { children, ...restProps } = this.props;
+    const { appLocale } = this.state;
+
+    // Temp remove SentryBoundary
+    return (
+      <IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
+        {/* <LocaleProvider locale={appLocale.locale === 'zh-CN' ? zhCN : null}> */}
+        {/* <LocaleProvider locale={appLocale.locale === 'zh-CN' ? null : null}>
+          <div className="page-wrapper">
+            <Header {...restProps} />
+            {children}
+          </div>
+        </LocaleProvider> */}
+        <div>11111111111</div>
+      </IntlProvider>
+    );
+  }
+}
