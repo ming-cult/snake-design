067a3b5d723817af2e06fb9ae0fd1536105934e0
MuYunyun <muyy95@gmail.com>
2019-05-04 19:36:18 +0800

    Doc/affix (#38)
    * feat: temp
    
    * feat: markdown 中可以展示组件
    
    * feat: snake-design 样式添加
    
    * fix: fix #14 fix #13
    
    * docs: 文档增加 affix 布局

--- docs/layout/docsNav.tsx
+++ docs/layout/docsNav.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { NavLink } from 'react-router-dom'
+import { Affix } from 'components/index'
 
 interface ComponentProps {
   name: string
@@ -13,31 +14,32 @@ interface NavProps {
 }
 
 export default class DocsNav extends React.Component<NavProps, any> {
-
   // 渲染左侧导航
   renderNavList = (list: Array<ComponentProps>) => {
     return list.map((item, index) => {
       if (item.isDevelopmenting && process.env.NODE_ENV !== 'localdev') {
         return null
       }
-      return <NavLink
-        key={index}
-        activeClassName="sub-active"
-        to={item.path}
-      >
-        {item.name}
-      </NavLink>
+      return (
+        <NavLink key={index} activeClassName="sub-active" to={item.path}>
+          {item.name}
+        </NavLink>
+      )
     })
   }
 
   render() {
     const { basicCList, aboutSnakeDesignList } = this.props
     return (
-      <div className="snake-design-doc-nav">
-        <div>
+      <Affix offsetTop={0}>
+        <div className="snake-design-doc-nav">
           <section className="each-section">
             <header>快速开始</header>
-            <div className="sub">{this.renderNavList(aboutSnakeDesignList.filter(item => item.path.match(/\/guide\//)))}</div>
+            <div className="sub">
+              {this.renderNavList(
+                aboutSnakeDesignList.filter(item => item.path.match(/\/guide\//))
+              )}
+            </div>
           </section>
           <section className="each-section">
             <header>基础组件</header>
@@ -45,10 +47,12 @@ export default class DocsNav extends React.Component<NavProps, any> {
           </section>
           <section className="each-section">
             <header>更多信息</header>
-            <div className="sub">{this.renderNavList(aboutSnakeDesignList.filter(item => item.path.match(/\/more\//)))}</div>
+            <div className="sub">
+              {this.renderNavList(aboutSnakeDesignList.filter(item => item.path.match(/\/more\//)))}
+            </div>
           </section>
         </div>
-      </div>
+      </Affix>
     )
   }
 }
